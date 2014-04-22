756fc009f0aef950fe800b00b91e17170e7ba1cf
Shashank Ramaprasad <shashank.ramaprasad@gmail.com>
2014-04-22 15:15:39 -0700

    stylez

--- viz.js
+++ viz.js
@@ -29,7 +29,10 @@ var force = d3.layout.force()
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .attr("fill", "none")
+    .attr("stroke", "grey")
+    .attr("stroke-width", 2);
 
 // build the arrow.
 svg.append("svg:defs").selectAll("marker")
