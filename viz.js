133095d106951cc3188f172c95e5473537072f9f
Shashank Ramaprasad <shashank.ramaprasad@gmail.com>
2014-05-01 01:11:31 -0700

    node sizes now scaled proportional to number of incident edges; arcs redrawn, and text re-placed accordingly

--- viz.js
+++ viz.js
@@ -1,4 +1,3 @@
-
 // get the data
 d3.csv("schema.csv", function(error, links) {
 
@@ -6,16 +5,23 @@ var nodes = {};
 
 // Compute the distinct nodes from the links.
 links.forEach(function(link) {
-    // link.source is guaranteed to be non-null
-    nodes[link.source] || (nodes[link.source] = {name: link.source});
-    if (link.target) {
-        link.source = nodes[link.source];
-        link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
-    }
-});
+	// link.source is guaranteed to be non-null
+	nodes[link.source] || (nodes[link.source] = {name: link.source, edges: 0});
+	if (link.target) {
+	    link.source = nodes[link.source];
+	    link.target = nodes[link.target] || (nodes[link.target] = {name: link.target, edges: 0});
+            link.source.edges += 1;
+            link.target.edges += 1;
+	}
+    });
 
-var width = 1200,
-    height = 600;
+// bounding box for the svg container
+var width = 2500,
+    height = 2500;
+
+// circle radii
+var min_r = 5,
+    max_r = 20;
 
 var force = d3.layout.force()
     .nodes(d3.values(nodes))
@@ -36,14 +42,13 @@ svg.append("svg:defs").selectAll("marker")
     .data(["end"])      // Different link/path types can be defined here
   .enter().append("svg:marker")    // This section adds in the arrows
     .attr("id", String)
-    .attr("viewBox", "0 -5 10 10")
-    .attr("refX", 15)
-    .attr("refY", -1.5)
+    .attr("refX", 5)
+    .attr("refY", 3)
     .attr("markerWidth", 6)
     .attr("markerHeight", 6)
     .attr("orient", "auto")
   .append("svg:path")
-    .attr("d", "M0,-5L10,0L0,5");
+    .attr("d", "M0,0 L0,6 L6,3 Z");
 
 // add the links and the arrows
 var path = svg.append("svg:g").selectAll("path")
@@ -59,28 +64,36 @@ var node = svg.selectAll(".node")
     .attr("class", "node")
     .call(force.drag);
 
+var rscale = d3.scale.linear()
+    .domain([0, d3.max( force.nodes(), function(d) { return d.edges; } )])
+    .range([min_r, max_r]);
+
 // add the nodes
 node.append("circle")
-    .attr("r", 5);
+    .attr("r", function(d) { return rscale(d.edges);} );
 
 // add the text 
 node.append("text")
-    .attr("x", 12)
+    .attr("x", function(d) { return rscale(d.edges) + 3; })
     .attr("dy", ".35em")
     .text(function(d) { return d.name; });
 
 // add the curvy lines
 function tick() {
     path.attr("d", function(d) {
-        var dx = d.target.x - d.source.x,
-            dy = d.target.y - d.source.y,
-            dr = Math.sqrt(dx * dx + dy * dy);
+	    var dx = d.target.x - d.source.x,
+		dy = d.target.y - d.source.y,
+		dr = Math.sqrt(dx * dx + dy * dy);
+	    var n = rscale(d.target.edges),   // radius of target circle
+		k = n / dr;                   // multiplier
+	    var x2 = (1 - k) * d.target.x + k * d.source.x;
+	    var y2 = (1 - k) * d.target.y + k * d.source.y;
         return "M" + 
             d.source.x + "," + 
             d.source.y + "A" + 
             dr + "," + dr + " 0 0,1 " + 
-            d.target.x + "," + 
-            d.target.y;
+            x2 + "," + 
+            y2;
     });
 
     node
