23115b7d3dd34ba7a73e396f11eed2f94a68987f
Shashank Ramaprasad <shashankr@me.com>
2022-06-30 21:38:07 -0700

    added a pre-commit hook to run jest tests, and added more tests to try out this new hook

--- src/tests/parse.test.js
+++ src/tests/parse.test.js
@@ -1,5 +1,46 @@
 import parse from "../parse";
 
-test("parse returns empty when input is empty", () => {
+test("parse returns empty map when input is empty", () => {
   expect(parse([])).toStrictEqual({ nodes: [], links: [] });
+});
+
+test("parse when input is just one edge and one record", () => {
+  expect(parse([{ source: "team", target: "player" }])).toStrictEqual({
+    nodes: [
+      { name: "team", edges: 1 },
+      { name: "player", edges: 1 },
+    ],
+    links: [{ source: "team", target: "player" }],
+  });
+});
+
+test("parse when input is just one edge, but two records", () => {
+  expect(
+    parse([{ source: "team", target: "player" }, { source: "player" }])
+  ).toStrictEqual({
+    nodes: [
+      { name: "team", edges: 1 },
+      { name: "player", edges: 1 },
+    ],
+    links: [{ source: "team", target: "player" }],
+  });
+});
+
+test("parse when input is a chain of dependencies", () => {
+  expect(
+    parse([
+      { source: "team", target: "player" },
+      { source: "league", target: "team" },
+    ])
+  ).toStrictEqual({
+    nodes: [
+      { name: "team", edges: 2 },
+      { name: "player", edges: 1 },
+      { name: "league", edges: 1 },
+    ],
+    links: [
+      { source: "team", target: "player" },
+      { source: "league", target: "team" },
+    ],
+  });
 });
