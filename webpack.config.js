e596db2601b354aba3ab487be5ffa25c36e2a6dd
Shashank Ramaprasad <shashankr@me.com>
2022-07-04 14:06:15 -0700

    Prettier formatting for the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, "./src/index.js"),
   module: {
     rules: [
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+        use: ["babel-loader"],
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js']
+    extensions: ["*", ".js"],
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
   },
   devServer: {
-    static: path.resolve(__dirname, './dist'),
-  }
+    static: path.resolve(__dirname, "./dist"),
+  },
 };
