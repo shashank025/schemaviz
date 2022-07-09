f3a8c4ad8e6114ec8a76ec3d97bcb9f1c87d6354
Shashank Ramaprasad <shashankr@me.com>
2022-07-08 19:25:22 -0700

    prevent page reload if user hits Enter when focused on the text input form field

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,10 +10,13 @@ const API_ENDPOINT = "http://localhost:8000/dependencies";
 main();
 
 function main() {
+  const visualizeButton = document.querySelector("#myBtn");
+
   // when the "Visualize" button is clicked
-  document.querySelector("#myBtn").addEventListener("click", () => {
+  visualizeButton.addEventListener("click", () => {
     const connUri = document.getElementById("connectionUri").value;
     const encodedConnUri = encodeURIComponent(connUri);
+    // TODO: start a spinner that stops when this call finishes
     axios
       .get(`${API_ENDPOINT}?connection_uri=${encodedConnUri}`)
       .then(response => {
@@ -22,8 +25,18 @@ function main() {
         renderer.render();
       })
       .catch(error => {
+        // TODO: show nice error message
         console.log(error);
       });
   });
-  // TODO: handle Enter button press in the text input box
+
+  // when user presses Enter key with focus in the text input form field
+  document.querySelector("#connectionUri").addEventListener("keypress", e => {
+    if (e.key === "Enter") {
+      // we dont want to reload the page, dawg!
+      e.preventDefault();
+      // but we do want to simulate a button click!
+      visualizeButton.click();
+    }
+  });
 }
